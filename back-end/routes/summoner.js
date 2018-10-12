1d6361f96207e3503f5840bd3022992d8680a5ef
seyoon.kim <ksy7533@zuminternet.com>
2018-10-12 16:30:18 +0900

    summoner router 추가

---
+++ back-end/routes/summoner.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+    const { nick_name } = req.body;
+    res.redirect('/summoner/score/' + nick_name);
+});
+
+router.get('/score/:nick_name', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../public/bundle/dist', 'index.html'))
+});
+
+module.exports = router;
